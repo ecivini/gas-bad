8b441caddf5d132a6de7d33522511d36edff4a4b
Zaryab <zaryabafser2000@gmail.com>
2023-06-21 19:29:37 +0400

    added rolebasedAccess

---
+++ src/RoleBasedAccess/OZRoleAccess.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "lib/openzeppelin-contracts/contracts/access/AccessControl.sol";
+
+contract OZRoleAccess is AccessControl {
+  /**
+    * @notice Constructor
+    */
+  constructor () { }
+}
---
+++ src/RoleBasedAccess/SoladyRoleAccess.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "lib/solady/src/auth/OwnableRoles.sol";
+
+contract SoladyRoleAccess is OwnableRoles {
+  /**
+    * @notice Constructor
+    */
+  constructor () { }
+}
