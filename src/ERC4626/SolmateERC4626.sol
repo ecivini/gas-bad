70206b418ad3ad4aa2d4aff64dcc89b6ecd63f83
Egor Dergunov <sqrlfirst@Egors-MacBook-Pro.local>
2023-10-24 17:03:57 +0700

    start adding erc4626 feature

---
+++ src/ERC4626/SolmateERC4626.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "lib/solady/src/mixins/ERC4626.sol";
+
+contract SolmateERC4626 is ERC4626 {
+    
+    /**
+     * @notice Constructor
+     */
+    constructor () ERC4626("Test4626", "TEST4626") { 
+        _mint(msg.sender, 0);
+        _mint(msg.sender, 1);
+        _mint(msg.sender, 2);
+        _mint(msg.sender, 3);
+        _mint(msg.sender, 5);
+    }
+
+    /**
+     * @notice Returns the name of the token
+     */
+    function name() public pure override returns (string memory) {
+        return "Test";
+    }
+
+    /**
+     * @notice Returns the symbol of the token
+     */
+    function symbol() public pure override returns (string memory) {
+        return "TEST";
+    }
+
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return "ipfs://...";
+    }
+
+    /**
+     * @notice Mint a token to the specified address
+     * @param _to Receiver address
+     * @param _tokenId Id of the token to mint
+     */
+    function mint(address _to, uint _tokenId) external {
+        _mint(_to, _tokenId);
+    }
+
+    /**
+     * @notice Safely mint a token to the specified address
+     * @param _to Receiver address
+     * @param _tokenId Id of the token to mint
+     */
+    function safeMint(address _to, uint _tokenId) external {
+        _safeMint(_to, _tokenId);
+    }
+
+    /**
+     * @notice Burn a token
+     * @param _tokenId Id of the token to burn
+     */
+    function burn(uint _tokenId) external {
+        _burn(_tokenId);
+    }
+
+    /// _baseURI override
+    function _baseURI() internal view override returns (string memory) {
+        return "ipfs://.../";
+    }
+}
